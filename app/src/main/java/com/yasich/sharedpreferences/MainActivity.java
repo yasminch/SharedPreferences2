4397e90ddd97828a03418809d5044f746d61e6e3
yasminch <jessymina.jc@gmail.com>
2019-12-02 14:13:48 +0100

    Initial commit

---
+++ app/src/main/java/com/yasich/sharedpreferences/MainActivity.java
@@ -0,0 +1,80 @@
+package com.yasich.sharedpreferences;
+
+import android.app.Activity;
+import android.content.Context;
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.View;
+import android.widget.TextView;
+
+import com.yasich.sharedpreferences.R;
+
+import static com.yasich.sharedpreferences.R.*;
+
+public class MainActivity extends Activity {
+    SharedPreferences sharedpreferences;
+    TextView name;
+    TextView email;
+    public static final String mypreference = "mypref";
+    public static final String Name = "nameKey";
+    public static final String Email = "emailKey";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(layout.activity_main);
+        name = (TextView) findViewById(id.etName);
+        email = (TextView) findViewById(id.etEmail);
+        sharedpreferences = getSharedPreferences(mypreference,
+                Context.MODE_PRIVATE);
+        if (sharedpreferences.contains(Name)) {
+            name.setText(sharedpreferences.getString(Name, ""));
+        }
+        if (sharedpreferences.contains(Email)) {
+            email.setText(sharedpreferences.getString(Email, ""));
+
+        }
+
+    }
+
+    public void Save(View view) {
+        String n = name.getText().toString();
+        String e = email.getText().toString();
+        SharedPreferences.Editor editor = sharedpreferences.edit();
+        editor.putString(Name, n);
+        editor.putString(Email, e);
+        editor.commit();
+    }
+
+    public void clear(View view) {
+        name = (TextView) findViewById(id.etName);
+        email = (TextView) findViewById(id.etEmail);
+        name.setText("");
+        email.setText("");
+
+    }
+
+    public void Get(View view) {
+        name = (TextView) findViewById(id.etName);
+        email = (TextView) findViewById(id.etEmail);
+        sharedpreferences = getSharedPreferences(mypreference,
+                Context.MODE_PRIVATE);
+
+        if (sharedpreferences.contains(Name)) {
+            name.setText(sharedpreferences.getString(Name, ""));
+        }
+        if (sharedpreferences.contains(Email)) {
+            email.setText(sharedpreferences.getString(Email, ""));
+
+        }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+}
